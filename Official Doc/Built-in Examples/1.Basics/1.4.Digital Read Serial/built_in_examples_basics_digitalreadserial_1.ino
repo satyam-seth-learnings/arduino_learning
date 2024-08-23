bf8a1a77c1bbda26adda8a5f1f55966148936c8c
Satyam Seth <satyam1998.1998@gmail.com>
2024-08-24 00:32:39 +0530

    rename basics example code files

--- Official Doc/Built-in Examples/1.Basics/1.4.Digital Read Serial/built_in_examples_basics_digitalreadserial_1.ino
+++ Official Doc/Built-in Examples/1.Basics/1.4.Digital Read Serial/built_in_examples_basics_digitalreadserial_1.ino
@@ -12,8 +12,7 @@
 int pushButton = 2;
 
 // the setup routine runs once when you press reset:
-void setup()
-{
+void setup() {
   // initialize serial communication at 9600 bits per second:
   Serial.begin(9600);
   // make the pushbutton's pin an input:
@@ -21,11 +20,10 @@ void setup()
 }
 
 // the loop routine runs over and over again forever:
-void loop()
-{
+void loop() {
   // read the input pin:
   int buttonState = digitalRead(pushButton);
   // print out the state of the button:
   Serial.println(buttonState);
-  delay(1); // delay in between reads for stability
+  delay(1);  // delay in between reads for stability
 }
